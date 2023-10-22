f4b9cceeeda06e093ad6f36ae931ed44aaa6d9e2
AgediSmagedi <148704939+AgediSmagedi@users.noreply.github.com>
2023-10-22 19:09:53 +0200

    Create update.lua

---
+++ update.lua
@@ -0,0 +1,14 @@
+print("data will be erased! continue?(y/n)/")
+if read() == y then
+
+print("starting update...")
+fs.delete("agedi")
+fs.delete("setup")
+fs.delete("startup")
+print("downloading setup.lua...")
+local setup = fs.open("setup", "w")
+local text = http.get("https://raw.githubusercontent.com/AgediSmagedi/cc-ag/main/setup.lua").readAll()
+setup.write(text)
+setup.close()
+print("now run setup")
+else print("aight") end
