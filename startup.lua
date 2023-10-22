618b25e297ceffd208f1d28c6640a3d5f554b4cc
AgediSmagedi <148704939+AgediSmagedi@users.noreply.github.com>
2023-10-22 17:13:53 +0200

    Created startup.lua

---
+++ startup.lua
@@ -0,0 +1,33 @@
+password = "cheese"
+signedin = false
+
+ag = require("ag")
+ag.clear("-- welcome --")
+
+
+local function pass()
+    
+    write("enter password/ ")
+
+
+    while signedin == false do
+    if read("*") == password then
+        print("welcome")
+        signedin = true
+    else
+        write("try again!/ ")
+    end
+    end
+ag.clear("------------- Welcome Back --------------")
+end
+
+local function offlock()
+    while true do
+        local event = os.pullEventRaw("terminate")
+        if event == "terminate" then
+            os.shutdown()
+        end
+    end
+end
+
+parallel.waitForAny(offlock, pass)
