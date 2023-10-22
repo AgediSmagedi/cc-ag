204142e9bc55dd623a3e8d1a764cf0f19c6b6e86
AgediSmagedi <148704939+AgediSmagedi@users.noreply.github.com>
2023-10-22 19:23:53 +0200

    Update setup.lua

--- setup.lua
+++ setup.lua
@@ -1,19 +1,29 @@
 print("starting install...")
-local update = fs.open("update", "w")
-if update ~= nil then
+
+--gotta remove update file if its there
+local isUpdate = fs.open("update", "w")
+if isUpdate ~= nil then
     fs.delete("update")
 end
 local agUrl = "https://raw.githubusercontent.com/AgediSmagedi/cc-ag/main/ag.lua"
 local startUrl = "https://raw.githubusercontent.com/AgediSmagedi/cc-ag/main/startup.lua"
+local updateUrl = "https://raw.githubusercontent.com/AgediSmagedi/cc-ag/main/update.lua"
 
 agedi = fs.makeDir("agedi")
+
 print("downloading ag.lua...")
 local ag = fs.open("agedi/ag", "w")
 local text = http.get(agUrl).readAll()
 ag.write(text)
 ag.close()
 
-print("downloading startup.lua...")
+print("downloading update.lua...")
+local update = fs.open("agedi/update", "w")
+text = http.get(updateUrl).readAll()
+update.write(text)
+update.close()
+
+print("downloading startup.lua")
 local startup = fs.open("startup", "w")
 text = http.get(startUrl).readAll()
 startup.write(text)
